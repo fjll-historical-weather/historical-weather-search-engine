9b59f11091fae722a57ecd1e2cba181d5688ab51
leilani <lherna05@gmail.com>
2022-08-12 20:01:09 -0700

    added navbar, search bar, setup basic components and styling

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,26 @@
+//client/components/Navbar.jsx
+
+import React from 'react'; 
+import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
+
+const Navbar = props => {
+
+  return (
+    <div className="navbar">
+      <button>Home</button>
+      <button>Login/Sign Up</button>
+    </div>
+  )
+
+}
+
+
+
+
+
+
+
+
+
+export default Navbar;
