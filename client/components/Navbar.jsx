77f62b88c57cc95b7dbc5541fa98564f4bdd6486
Jonathan Chen <jonathan.hp.chen@gmail.com>
2022-08-15 18:20:53 -0700

    Added some conditional rendering to login page

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,17 +1,23 @@
 //client/components/Navbar.jsx
-import React from 'react'; 
+import React , {useState, useEffect} from 'react'; 
 import { Link } from 'react-router-dom';
 
 const Navbar = props => {
 
   // if isLoggedIn = true, render x
+  const [isLoggedIn, setIsLoggedIn] = useState(props.isLoggedIn)
+
+  // useEffect(()=>{
+  //   console.log('Navbar.jsx isLoggedIn: ', isLoggedIn)
+  // }, [isLoggedIn])
 
   return (
     <div className="navbar">
       <Link to='/' className="submit-btn">Home</Link>
-      <h1>Historial Climate Data</h1>
-      <Link to='/signup' className="submit-btn">Signup</Link>
-      <Link to='/login' className="submit-btn">Login</Link>
+      <h1>Historical Climate Data</h1>
+      {/* <Link to='/signup' className="submit-btn">Signup</Link> */}
+      <Link to='/login' className="submit-btn">Signup/Login</Link>
+      {isLoggedIn ? <Link to='/login' className="submit-btn">Logout</Link> : <div></div>} 
     </div>
   )
 
